f9d74b8548bebdf6d42b12ee7feb1c0b84241227
Siri Bafna <Siri.Bafna@improving.com>
2019-09-19 10:08:01 -0500

    constructors and person

---
+++ src/main/java/org/improving/tag/Person.java
@@ -0,0 +1,37 @@
+package org.improving.tag;
+
+public class Person {
+
+    private String name, dateOfBirth, address, city, state;
+    int zip;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDateOfBirth() {
+
+        return dateOfBirth;
+    }
+
+    public String getAddress() {
+
+        return address;
+    }
+
+    public String getCity() {
+
+        return city;
+    }
+
+    public String getState() {
+
+        return state;
+    }
+
+    public int getZip() {
+
+        return zip;
+    }
+
+}
