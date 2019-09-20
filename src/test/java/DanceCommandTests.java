9a4397b3a3a511eed66e70edd7c31b409eba6cd6
Siri Bafna <Siri.Bafna@improving.com>
2019-09-20 16:35:29 -0500

    Unit Testing Started

---
+++ src/test/java/DanceCommandTests.java
@@ -0,0 +1,73 @@
+import org.improving.tag.InputOutput;
+import org.improving.tag.commands.DanceCommand;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DanceCommandTests {
+
+    DanceCommand target;
+    TestInputOutput io;
+
+    @BeforeEach
+    public void arrange() {
+
+        io = new TestInputOutput();
+        target = new DanceCommand(io);
+    }
+
+    @Test
+    public void execute_should_return_phrase() {
+        // act
+        target.execute(null);
+
+        //
+        assertEquals("You dance around.",  io.lastText);
+
+    }
+
+    @Test
+    public void isValid_should_be_true_when_input_is_dance() {
+
+        // Act
+        var result = target.isValid("dance");
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValid_should_be_true_when_input_is_dance_with_spaces() {
+
+        // Act
+        var result = target.isValid("  dance   ");
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValid_should_be_true_when_input_is_dance_with_caps() {
+
+        // Act
+        var result = target.isValid("dANCe");
+        // Assert
+        assertTrue(result);
+    }
+
+    @Test
+    public void isValid_should_be_false_when_input_is_foobar() {
+
+        var result = target.isValid("foobar");
+        // Assert
+        assertFalse(result);
+    }
+
+    @Test
+    public void isValid_should_be_false_when_input_is_null() {
+
+        var result = target.isValid(null);
+        // Assert
+        assertFalse(result);
+    }
+}
+
